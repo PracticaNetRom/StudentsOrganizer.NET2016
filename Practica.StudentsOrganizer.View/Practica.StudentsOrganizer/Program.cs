d357a9706f6d39ccb47df8e28fb7f83501754637
RoxanaMarin <marin.roxana07@gmail.com>
2016-07-04 12:28:13 +0300

    4/7/2016 ok

--- Practica.StudentsOrganizer.View/Practica.StudentsOrganizer/Program.cs
+++ Practica.StudentsOrganizer.View/Practica.StudentsOrganizer/Program.cs
@@ -17,6 +17,40 @@ namespace Practica.StudentsOrganizer
         [STAThread]
         static void Main()
         {
+            /*Students_BO Std = new Students_BO();
+            Std.First_Name = "Maria";
+            Std.Last_Name = "Popescu";
+            Std.Gender = "Female";
+            Std.Birth_Date = new DateTime(1992, 02, 15);
+            Std.Email = "Mar@hdy.com";
+            Std.Phone_Number = "0756023048";
+            Std.Faculty = "Ace";
+            Std.Faculty_Start_Year = 2014;
+            Std.Remarks = "hjgfjuehfuheuhfie";
+
+
+
+            Students_DAO Std_DAO = new Students_DAO();
+            Std_DAO.AddStudent(Std);
+
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString =
+            "Data Source=netsrv-db01\\sql2014;" +
+            "Initial Catalog=NetRom.Practice4;" +
+            "Integrated Security=SSPI;";
+
+            /*"Data Source=ROXXANA\\SQLEXPRESS;" +
+            "Initial Catalog=PracticaNETROM;" +
+            "Integrated Security=SSPI;";*/
+
+           /* conn.Open();
+
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                MessageBox.Show("ok");
+            };
+            */
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new AddStudentForm());
