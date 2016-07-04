8c8e6bf074a5d99f4f77ca3b9940baacbdc0d070
cameliagongea <gongea.camelia@yahoo.com>
2016-07-04 09:42:58 +0300

    Revert "Revert "TemaDuminica""
    This reverts commit ebea97a891facdcd04c678ab89861f7d51528da0.

---
+++ Practica.StudentsOrganizer/Practica.StudentsOrganizer.Model/BO/StudentEventOccurenceBO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica.StudentsOrganizer.Model.BO
+{
+    class StudentEventOccurenceBO
+    {
+        public int id { get; set; }
+        public int idStudent { get; set; }
+        public int idEvenOccurence { get; set; }
+    }
+}
