b5b925e02714456744f35eb6b33c0572b21203a0
DragosPuscu <puscu.dragos@yahoo.com>
2016-07-05 23:56:28 +0300

    tema-1

--- Practica.StudentsOrganizer.View/Practica.StudentsOrganizer.Model/StudentDAO.cs
+++ Practica.StudentsOrganizer.View/Practica.StudentsOrganizer.Model/StudentDAO.cs
@@ -13,7 +13,7 @@ namespace Practica.StudentsOrganizer.Model
     {
         private object sqlDbType;
 
-        public evenimentBO GetStud(int Id)
+        public studentBO GetStud(int Id)
         {
             SqlConnection conn = new SqlConnection();
             conn.ConnectionString = "Data Source=netsrv-db01\\sql2014;" +
@@ -31,7 +31,7 @@ namespace Practica.StudentsOrganizer.Model
             {
                 if (reader.HasRows)//intoarce daca sunt sau nu randuri de citit
                 {
-                    evenimentBO GetStud = new evenimentBO();
+                    studentBO GetStud = new studentBO();
                     GetStud.FirstName = reader["FirstName"].ToString();
                     GetStud.LastName = reader["LastName"].ToString();
                     GetStud.Gender = reader["Gender"].ToString();
