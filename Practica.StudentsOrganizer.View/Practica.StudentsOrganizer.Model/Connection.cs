63865d7bf8df01310ba7a57e6a49c9bcb8c48e8d
DragosPuscu <puscu.dragos@yahoo.com>
2016-07-11 23:33:10 +0300

    modificare Conect

--- Practica.StudentsOrganizer.View/Practica.StudentsOrganizer.Model/Connection.cs
+++ Practica.StudentsOrganizer.View/Practica.StudentsOrganizer.Model/Connection.cs
@@ -8,8 +8,16 @@ namespace Practica.StudentsOrganizer.Model
 {
     class Connection
     {
-      public const string ConValue= "Data Source=netsrv-db01\\sql2014;" +
-            "Initial Catalog=NetRom.Practice5;" +
-            "Integrated Security=SSPI;";
+        //Connect Firma.
+      //public const string ConValue= "Data Source=netsrv-db01\\sql2014;" +
+         //   "Initial Catalog=NetRom.Practice5;" +
+          //  "Integrated Security=SSPI;";
+
+
+            //Connect HOME.
+        public const string ConValue = "Data Source=VOSTRO15\\SQLEXPRESS;" +
+      "Initial Catalog=Net-Rom;" +
+      "Integrated Security=SSPI;";
+
     }
 }
