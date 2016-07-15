3563b23395d198c183335f634cd67e0f745c0cb9
DragosPuscu <puscu.dragos@yahoo.com>
2016-07-15 10:48:19 +0300

    Finall Project

--- Practica.StudentsOrganizer.View/Practica.StudentsOrganizer.Model/Connection.cs
+++ Practica.StudentsOrganizer.View/Practica.StudentsOrganizer.Model/Connection.cs
@@ -9,15 +9,15 @@ namespace Practica.StudentsOrganizer.Model
     class Connection
     {
         //Connect Firma.
-            public const string ConValue= "Data Source=netsrv-db01\\sql2014;" +
-            "Initial Catalog=NetRom.Practice5;" +
-            "Integrated Security=SSPI;";
+      //      public const string ConValue= "Data Source=netsrv-db01\\sql2014;" +
+        //    "Initial Catalog=NetRom.Practice5;" +
+          //  "Integrated Security=SSPI;";
 
 
             //Connect HOME.
-       //public const string ConValue = "Data Source=VOSTRO15\\SQLEXPRESS;" +
-      //"Initial Catalog=Net-Rom;" +
-      //"Integrated Security=SSPI;";
+       public const string ConValue = "Data Source=VOSTRO15\\SQLEXPRESS;" +
+      "Initial Catalog=Net-Rom;" +
+      "Integrated Security=SSPI;";
 
     }
 }
